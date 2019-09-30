6fd607cff3989bd0a1c58c1eaf86e9776d46190a
Jolin <susan8213@hotmail.com.tw>
2019-09-30 22:48:34 +0800

    Expressions and Statements

---
+++ 04 Java Tutorial - Expressions, Statements, Code blocks, Methods and more/KeywordsAndExpressions/src/academy/learnprogramming/Main.java
@@ -0,0 +1,24 @@
+package academy.learnprogramming;
+
+/*
+    Keywords: https://en.wikipedia.org/wiki/List_of_Java_keywords
+    Expressions
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Keywords
+        // int int=5;  // error
+        // false, true, null
+
+        // Expressions
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);  // (100 * 1.609344) is expression
+        int highScore = 50;  // highScore = 50 is expression
+        if (highScore == 50) {  // (highScore == 50) is expression
+            System.out.println("This is an expression");  // "This is an expression" is expression
+        }
+    }
+}
---
+++ 04 Java Tutorial - Expressions, Statements, Code blocks, Methods and more/StatementsWhiteSpaceAndIndenting/src/academy/learnprogramming/Main.java
@@ -0,0 +1,26 @@
+package academy.learnprogramming;
+
+/*
+    Statements
+        use whitespace and indenting to make you easy to read the code logic.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // 1 Statement in one line
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test.");
+
+        // 1 Statement can be written in many lines.
+        System.out.println("This is" +
+                "another" +
+                "still more.");
+
+        // Statements cane be written in one line, bit not easy to read.
+        int anotherVariable = 50; myVariable--; System.out.println("This is another one.");
+    }
+}
