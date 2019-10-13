bded9a59888f0b2cb9e3a63515d8096c7b4534f4
Jolin <susan8213@hotmail.com.tw>
2019-10-13 10:12:02 +0800

    Exercise: Classes & Constructors

---
+++ 06 OOP Part 1 - Classes, Constructor and Inheritance/Exercises/src/academy/learnprogramming/Main.java
@@ -0,0 +1,73 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // SimpleCalculator
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        simpleCalculator.setFirstNumber(5.0);
+        simpleCalculator.setSecondNumber(4);
+        System.out.println("add= " + simpleCalculator.getAdditionResult());
+        System.out.println("subtract= " + simpleCalculator.getSubtractionResult());
+        simpleCalculator.setFirstNumber(5.25);
+        simpleCalculator.setSecondNumber(0);
+        System.out.println("multiply= " + simpleCalculator.getMultiplicationResult());
+        System.out.println("divide= " + simpleCalculator.getDivisionResult());
+
+        // Person
+        Person person = new Person();
+        person.setFirstName("");   // firstName is set to empty string
+        person.setLastName("");    // lastName is set to empty string
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");    // firstName is set to John
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");    // lastName is set to Smith
+        System.out.println("fullName= " + person.getFullName());
+
+        // Wall
+        Wall wall = new Wall(5,4);
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
+
+        // Point
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+        Point point = new Point();
+        System.out.println("distance()= " + point.distance());
+
+        // CarpetCostCalculator
+        Carpet carpet = new Carpet(3.5);
+        Floor floor = new Floor(2.75, 4.0);
+        Calculator calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+        carpet = new Carpet(1.5);
+        floor = new Floor(5.4, 4.5);
+        calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+
+        // ComplexNumber
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        one.add(1,1);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        one.subtract(number);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        number.subtract(one);
+        System.out.println("number.real= " + number.getReal());
+        System.out.println("number.imaginary= " + number.getImaginary());
+    }
+}
