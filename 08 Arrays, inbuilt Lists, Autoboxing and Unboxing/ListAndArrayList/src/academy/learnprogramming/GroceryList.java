96e627f7dc78c94063052e2f0c5bb3f310da2abc
susan8213 <susan8213@gmail.com>
2019-10-31 22:36:15 +0800

    ArrayList

---
+++ 08 Arrays, inbuilt Lists, Autoboxing and Unboxing/ListAndArrayList/src/academy/learnprogramming/GroceryList.java
@@ -0,0 +1,62 @@
+package academy.learnprogramming;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    // private int[] myNumbers = new int[50];
+    private ArrayList<String> groceryList = new ArrayList<>();
+
+    public ArrayList<String> getGroceryList() {
+        return groceryList;
+    }
+
+    public void addGroceryItem(String item) {
+        // myNumber[0] = 1;
+        groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your grocery list");
+        for(int i=0; i<groceryList.size(); i++) {
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(String currentItem, String newItem) {
+        int position = findItem(currentItem);
+        if(position >= 0) {  // position = -1 when can't find the item in ArrayList
+            modifyGroceryItem(position, newItem);
+        }
+    }
+
+    private void modifyGroceryItem(int position, String newItem) {
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position+1) + " has been modified.");
+    }
+
+    public void removeGroceryItem(String item) {
+        int position = findItem(item);
+        if(position >=0) {  // position = -1 when can't find the item in ArrayList
+            removeGroceryItem(position);
+        }
+    }
+
+    private void removeGroceryItem(int position) {
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+    }
+
+    public int findItem(String searchItem) {
+        // boolean exists = groceryList.contains(searchItem);
+        return groceryList.indexOf(searchItem);
+    }
+
+    public boolean onFile(String searchItem) {
+        int position = findItem(searchItem);
+        if(position >= 0) {
+            return true;
+        }
+
+        return false;
+    }
+}
